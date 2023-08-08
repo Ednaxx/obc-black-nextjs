63ff223f0d0d9b3b9d00909a8a392f78f291d68f
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-08-08 14:05:34 -0300

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+
+        <nav>
+          <Link href={"/"} >Start</Link>
+          <Link href={"/products"} >Products</Link>
+          <Link href={"/cart"} >Cart</Link>
+        </nav>
+
         <Main />
         <NextScript />
       </body>
