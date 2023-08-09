59f9144b29ab8e8318bd5a4f4ac0ced8fe4bc75f
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-08-09 11:09:03 -0300

    server-side rendering video-class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-
-        <nav>
-          <Link href={"/"} >Start</Link>
-          <Link href={"/products"} >Products</Link>
-          <Link href={"/cart"} >Cart</Link>
-        </nav>
-
         <Main />
         <NextScript />
       </body>
